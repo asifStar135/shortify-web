e163187627dea05cb1155a1d17c1ddc81086349b
asifstar135 <asifstar135@gmail.com>
2026-08-31 14:23:46 +0530

    MVP build in progress. Completed : auth, create, list urls

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:shortCode",
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/url/get/:shortCode`,
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
